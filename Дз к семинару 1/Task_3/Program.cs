3230291bef24faf631c8f7e3a20efbd292dff860
Sheyko Dmitry <scheiko97dimon@gmail.com>
2022-05-06 20:17:56 +0300

    Добавил решение третьей  задачи

---
+++ Дз к семинару 1/Task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка). */
+
+Console.Write("Введите число: ");
+int a;
+a = Convert.ToInt32(Console.ReadLine());
+
+bool no = false;
+bool yes = true;
+
+Console.WriteLine(a % 2 == 0);
