ede5d24c8b673d9acb2981eedb37d85d4bd15781
Sheyko Dmitry <scheiko97dimon@gmail.com>
2022-05-08 15:30:38 +0300

    Добавил решение четвертой  задачи

---
+++ Дз к семинару 1/Task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая на вход
+принимает число (N), а на выходе показывает все чётные
+числа от 1 до N.*/
+Console.Clear();
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while (count <= number)
+{
+    if (count >= number - 1)
+    {
+        Console.Write(count);
+    }
+    else 
+    {
+        Console.Write(count + ", ");
+    }
+    count = count + 2;
+}
+
