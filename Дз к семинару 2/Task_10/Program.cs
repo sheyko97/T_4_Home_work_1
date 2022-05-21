1232bcb09a6ec3e00588748799b75676b5ce14da
Sheyko Dmitry <scheiko97dimon@gmail.com>
2022-05-21 17:47:14 +0300

    добавили task 10 к дз 2

---
+++ Дз к семинару 2/Task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int randomNumber = new Random().Next(100, 1000);
+Console.WriteLine(randomNumber);
+int numberOne = (randomNumber / 10);
+int numberTwo = (randomNumber / 100) * 10;
+Console.WriteLine($"{ numberOne - numberTwo }");
+
