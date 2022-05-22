36329910ce19603e764affe0fbe24b7d26abcf64
Sheyko Dmitry <scheiko97dimon@gmail.com>
2022-05-22 11:50:44 +0300

    добавили task 13 к дз 2

---
+++ Дз к семинару 2/Task_13/Program.cs
@@ -0,0 +1,18 @@
+﻿/* 
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 
+*/
+
+int randomNumber = new Random().Next(10,10000);
+Console.WriteLine(randomNumber);
+
+if (randomNumber < 99)
+{
+Console.WriteLine("третьей цифры нет");
+}
+else
+{
+ Console.WriteLine((randomNumber / 10) % 10);
+}
