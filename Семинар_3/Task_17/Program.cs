de88918231c9f5c8a599ee3d440c7303b0b44325
Sheyko Dmitry <scheiko97dimon@gmail.com>
2022-05-29 16:39:04 +0300

    добавил Console.Clear...

---
+++ Семинар_3/Task_17/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
+//  причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int GetNumberOfQuater(int X, int Y)
+{
+    int result = 0;
+    if (X > 0 && Y > 0)
+    {
+        result = 1;
+    }
+    else if (X < 0 && Y > 0)
+    {
+        result = 2;
+    }
+    if (X < 0 && Y < 0)
+    {
+        result = 3;
+    }
+    if (X > 0 && Y < 0)
+    {
+        result = 4;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите X");
+int userX = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Y");
+int userY = Convert.ToInt32(Console.ReadLine());
+
+int quater = GetNumberOfQuater(userX, userY);
+
+
+if (quater > 0)
+{
+Console.WriteLine($"Точка {userX}:{userY} находится в {quater} четверти");
+}
+else
+{
+Console.WriteLine($"Точка {userX}:{userY} находится  на пересечение четвертей");
+}
