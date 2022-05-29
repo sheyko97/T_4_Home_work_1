f7682b34770078bd8c79b9619a54dc4f5abfbdff
Sheyko Dmitry <scheiko97dimon@gmail.com>
2022-05-29 21:41:03 +0300

    добавил  решение задачи 23

--- Дз к семинару 3/Task_21/Program.cs
+++ Дз к семинару 3/Task_21/Program.cs
@@ -5,6 +5,8 @@
 Console.Clear();
 
 
+/*
+
 static void FindingDistanceBetweenPoints(int xa,int ya,int za, int xb, int yb, int zb)
 {
     double result = Math.Sqrt((xb - xa) * (xb - xa) + (yb - ya) * (yb - ya) + (zb - za) * (zb - za));
@@ -19,3 +21,31 @@ int xb = Convert.ToInt32(Console.ReadLine());
 int yb = Convert.ToInt32(Console.ReadLine());
 int zb = Convert.ToInt32(Console.ReadLine());
 FindingDistanceBetweenPoints(xa, ya, za, xb, yb, zb);
+*/
+
+
+void SearchForCoordinates(int[] CoordinatesA, int [] CoordinatesB)
+{
+    double result = 0;
+    for (int i = 0; i < CoordinatesA.Length; i++) 
+    {
+         result = result + Math.Pow((CoordinatesA[i]-CoordinatesB[i]),2);
+    }
+    Console.WriteLine($"A ({CoordinatesA[0]}, {CoordinatesA[1]}, {CoordinatesA[2]}) B ({CoordinatesB[0]}, {CoordinatesB[1]}, {CoordinatesB[2]}) - > {Math.Round(Math.Sqrt(result),2)}");
+}
+
+int [] arr1 = new int[3];
+Console.WriteLine("Введите координаты первой точки A (x, y, z): ");
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        arr1[i] = number; 
+    }
+    int [] arr2 = new int[3];
+Console.WriteLine("Введите координаты первой точки B (x, y, z): ");
+    for (int i = 0; i < arr2.Length; i++)
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        arr2[i] = number; 
+    }
+SearchForCoordinates(arr1,arr2);
---
+++ Дз к семинару 3/Task_23/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9.
+// 5 -> 1, 8, 27, 64, 125
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{number} - > ");
+if (number > 0)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        int result = Convert.ToInt32(Math.Pow(i, 3));
+        Console.Write(result);
+        if (i != number)
+        {
+            Console.WriteLine(" , ");
+        }
+    }
+}
+else
+{
+for (int i = -1; i >= number; i--)
+    {
+        int result = Convert.ToInt32(Math.Pow(i,3));
+        Console.Write(result);
+        if (i != number)
+        {
+            Console.Write(" , ");
+        }
+    }    
+}
