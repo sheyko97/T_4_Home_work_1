ebaedd036cd4c1af0c7d991b139f8fc03717ae04
Sheyko Dmitry <scheiko97dimon@gmail.com>
2022-06-04 15:16:23 +0300

    добавил решение 27 задачи

---
+++ Дз к семинару 4/Task_27/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+try
+{
+    int FindSumOfNumber(int number)
+    {
+        int result = 0;
+        for (int i = 0; i <= number; i++)
+        {
+            result = result + number % 10;
+            number = number / 10;
+        }
+        return result;
+    }
+
+    Console.WriteLine("Введите свое число - ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{num} - > {FindSumOfNumber(num)}");
+
+
+}
+catch
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Ошибка ввода! Вы ввели не числовое значение! повторите ввод.");
+    Console.ResetColor();
+}
