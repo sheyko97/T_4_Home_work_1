53e032b5c21892d7d8165e87225be737a01fc94d
Sheyko Dmitry <scheiko97dimon@gmail.com>
2022-06-07 22:49:31 +0300

    добавил 1-й вариант решения 29 задачи (еще не работает)

---
+++ Дз к семинару 4/Task_29/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+/*Я пока не смог дорешать эту задачу! :( */
+
+void MyArray(int[] array)
+{
+    for (int i = 0; i <= array.Length; i++)
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        array[i] = number;
+    }
+
+
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+        if (i != arr.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int result = PrintArray(8);
+Console.WriteLine($" {result}");
+
+
+
