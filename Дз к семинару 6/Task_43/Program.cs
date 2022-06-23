7570d64689f5724242a8ad8b95fd66ac02484f2e
Sheyko Dmitry <scheiko97dimon@gmail.com>
2022-06-23 19:19:58 +0300

    добавил решение задач 41,43

---
+++ Дз к семинару 6/Task_43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void intersectionPoint(double  b1, double  k1, double  b2, double  k2)
+{
+    if(k1!=k2)
+    {
+        double x = Math.Round(((b1 - b2)/(k2 - k1)),2);
+        double  y = Math.Round((k1*x+b1),2);
+        Console.WriteLine($"({x}; {y})");
+    }
+    else
+    {
+        Console.ForegroundColor=ConsoleColor.Red;
+        Console.WriteLine($"При введеных параметров: ({k1}) ({k2}) - линии не пересекутся");
+        Console.ResetColor();
+    }
+}
+int b1,k1,b2,k2;
+Console.WriteLine("Поиск точки пересечения двух прямых!\nВведите необходимые параметры b1, k1, b2 и k2: ");
+b1=Convert.ToInt32(Console.ReadLine());
+k1=Convert.ToInt32(Console.ReadLine());
+b2=Convert.ToInt32(Console.ReadLine());
+k2=Convert.ToInt32(Console.ReadLine());
+Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2= {k2}: ");
+intersectionPoint(b1, k1, b2, k2);
